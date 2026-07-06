47c7af9593abc27a95072e3bce5e8529fdae947b
Serhii <sergei.amvarcumov@gmail.com>
2026-07-06 20:52:26 +0200

    Add home page (index) view with basic statistics and visit counter

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import DishType, Cook, Dish
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_dishtypes = DishType.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
+    context = {
+        'num_dishtypes': num_dishtypes,
+        'num_cooks': num_cooks,
+        'num_dishes': num_dishes,
+        'num_visits': num_visits,
+    }
+
+    return render(request, "kitchen/index.html", context)
