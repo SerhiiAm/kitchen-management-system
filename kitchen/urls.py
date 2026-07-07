5ba1fe5c81d7b19f9554382378fb2f94ae6063c1
Serhii <sergei.amvarcumov@gmail.com>
2026-07-07 22:04:01 +0200

    Implement CRUD views and URLs for DishType model
    - Add DishTypeListView, CreateView, UpdateView, and DeleteView to kitchen/views.py
    
    - Register respective URL paths for DishType CRUD operations in kitchen/urls.py
    
    - Create temporary HTML templates for listing, creating/updating, and deleting dish types

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from kitchen.views import (index)
+from kitchen.views import (
+    index, DishTypeListView, DishTypeCreateView,
+    DishTypeUpdateView, DishTypeDeleteView,
+)
 
 app_name = "kitchen"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-type/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-type/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
+    path("dish-type/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish-type/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
 from kitchen.models import DishType, Cook, Dish
 
@@ -6,17 +9,43 @@ from kitchen.models import DishType, Cook, Dish
 def index(request):
     """View function for the home page of the site."""
 
-    num_dishtypes = DishType.objects.count()
+    num_dish_types = DishType.objects.count()
     num_cooks = Cook.objects.count()
     num_dishes = Dish.objects.count()
 
     num_visits = request.session.get('num_visits', 0) + 1
     request.session['num_visits'] = num_visits
     context = {
-        'num_dishtypes': num_dishtypes,
+        'num_dish_types': num_dish_types,
         'num_cooks': num_cooks,
         'num_dishes': num_dishes,
         'num_visits': num_visits,
     }
 
     return render(request, "kitchen/index.html", context)
+
+
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    context_object_name = "dish_types_list"
+    template_name = "kitchen/dish_type_list.html"
+
+
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+    template_name = "kitchen/dish_type_form.html"
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+    template_name = "kitchen/dish_type_form.html"
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    template_name = "kitchen/dish_type_confirm_delete.html"
+    success_url = reverse_lazy("kitchen:dish-type-list")
