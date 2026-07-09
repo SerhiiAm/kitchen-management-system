632c562b4cad91bb6b4acc6a28cbc14094a55f51
Serhii <sergei.amvarcumov@gmail.com>
2026-07-09 20:38:27 +0200

    Add DishSearchForm to forms.py - Add DishSearchForm with custom placeholder for searching dishes by name w

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -17,3 +17,14 @@ class DishForm(forms.ModelForm):
         fields = "__all__"
 
 
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
