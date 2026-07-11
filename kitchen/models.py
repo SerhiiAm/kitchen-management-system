b9b9c1f781474be25cbd1631a51c49c6090e24d3
Serhii <sergei.amvarcumov@gmail.com>
2026-07-11 19:36:06 +0200

    Add related_name='dishes' to ForeignKey in Dish model

--- kitchen/models.py
+++ kitchen/models.py
@@ -33,7 +33,7 @@ class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
     cooks = models.ManyToManyField(Cook, related_name="dishes")
 
     def __str__(self):
