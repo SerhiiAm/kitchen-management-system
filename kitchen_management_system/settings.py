877ea71761fb488d21119a8b710f3bc24d003b3a
Serhii <sergei.amvarcumov@gmail.com>
2026-07-15 20:31:28 +0200

    Configure environment variables for SECRET_KEY and DEBUG using python-dotenv

--- kitchen_management_system/settings.py
+++ kitchen_management_system/settings.py
@@ -11,18 +11,20 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-bcr!rqpqlfmh5k%g5cekllhg0vv4jty=y+tvqqfw7jrholb)+=")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = os.getenv("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -124,6 +126,6 @@ AUTH_USER_MODEL = "kitchen.Cook"
 
 LOGIN_REDIRECT_URL = "/"
 
-LOGOUT_REDIRECT_URL ="login"
+LOGOUT_REDIRECT_URL = "login"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
