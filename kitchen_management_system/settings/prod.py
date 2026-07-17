258381a5f309888b3829a808110373b3f818d997
Serhii <sergei.amvarcumov@gmail.com>
2026-07-17 21:19:09 +0200

    Refactor middleware structure and dynamic allowed hosts for production

--- kitchen_management_system/settings/prod.py
+++ kitchen_management_system/settings/prod.py
@@ -6,10 +6,17 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 DATABASES = {
     "default": dj_database_url.config(
